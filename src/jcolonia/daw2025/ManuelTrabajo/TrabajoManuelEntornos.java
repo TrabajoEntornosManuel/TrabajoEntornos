04f3efdd01a556999c4cd515bb6719118c2e629e
manuit1513-dev <manu.it1513@gmail.com>
2026-03-23 20:32:23 +0100

    este es mi segundo commit

--- src/jcolonia/daw2025/ManuelTrabajo/TrabajoManuelEntornos.java
+++ src/jcolonia/daw2025/ManuelTrabajo/TrabajoManuelEntornos.java
@@ -3,5 +3,6 @@ package jcolonia.daw2025.ManuelTrabajo;
 public class TrabajoManuelEntornos {
 	public static void main(String [] args) {
 		System.out.println("primer commit");
+		System.out.println("segundo commit");
 	}
 }
